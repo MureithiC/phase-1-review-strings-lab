18546f0410a39594f2188ec47fcda1ce51d62ae9
Cyrus Mureithi <cmraey@gmail.com>
2024-01-16 09:05:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Cyrus Mureithi';
+const  welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
